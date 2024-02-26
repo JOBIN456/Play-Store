d55acadc9837689c4960257f6606abb3dbc7ba0f
JOBIN456 <jobinj5210@gmail.com>
2024-02-26 20:49:55 +0530

    google

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,8 +1,48 @@
+// function showOrHideDiv() {
+//     var v = document.getElementById("dispearing");
+//     if (parseFloat(v.style.opacity) === 0) {
+//         v.style.opacity = "1";
+//     } 
+
+// }
+
+
+
 function showOrHideDiv() {
-    var v = document.getElementById("showOrHide");
-    if (v.style.display === "block") {
-       v.style.display = "none";
-    } else {
-       v.style.display = "block";
-    }
- }
+    var v = document.getElementById("dispearing");
+    v.style.display = 'none'; // Note the single quotes and the semicolon
+}
+
+// function showDiv() {
+//     var v = document.getElementById("dispearing");
+//     if (parseFloat(v.style.opacity) === 1) {
+//         v.style.opacity = "0";
+//     } else {
+//         v.style.opacity = "1";
+//     }
+// }
+
+ 
+// function showOrHideDiv() {
+//         var v = document.getElementById("searchid");
+//         if(v.style.opacity === 1)
+//         {
+//             v.style.opacity=0;
+//         } 
+//         else{
+//             v.style.opacity= 1;
+//         }
+//     }
+// $(document).ready(function()
+// {
+//     $('#btnclick').on('click',function(){
+//         $("#searchid").show();
+//     });
+    
+// });
+// $(document).ready(function()
+// {
+//     $('#btnclick').on('click',function(){
+//         $("#dispearing").toggle();
+//     });
+// });
