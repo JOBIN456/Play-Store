dd8f9d1f56b08120a0aaef4b4b23433b8b49de8d
JOBIN456 <jobinj5210@gmail.com>
2024-02-26 10:03:20 +0530

    first commit

---
+++ static/javascript/main.js
@@ -0,0 +1,8 @@
+function showOrHideDiv() {
+    var v = document.getElementById("showOrHide");
+    if (v.style.display === "block") {
+       v.style.display = "none";
+    } else {
+       v.style.display = "block";
+    }
+ }
