2067520563d9a3d0c8edc36dc89dcfba1031650a
JOBIN456 <jobinj5210@gmail.com>
2024-02-27 22:07:43 +0530

    march

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -62,3 +62,62 @@ function setOpacityToZero() {
 //         $("#dispearing").toggle();
 //     });
 // });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let scrollcontainer = document.querySelector(".home-gallery");
+//     let leftbtn = document.getElementById("left-button");
+//     let rightbtn = document.getElementById("right-button");
+
+//     rightbtn.addEventListener("click", () => {
+//         scrollcontainer.scrollLeft += 400;
+//     });
+
+//     leftbtn.addEventListener("click", () => {
+//         scrollcontainer.scrollLeft -= 400;
+//     });
+// });
+document.addEventListener("DOMContentLoaded", () => {
+    let scrollcontainer = document.querySelector(".home-gallery");
+    let leftbtn = document.getElementById("left-button");
+    let rightbtn = document.getElementById("right-button");
+
+    const imageWidth = 416;
+    const numberOfImagesToScroll = 3.2;
+    const scrollAmount = numberOfImagesToScroll * imageWidth;
+
+    rightbtn.addEventListener("click", () => {
+        document.documentElement.style.scrollBehavior = "smooth";
+        scrollcontainer.scrollLeft += scrollAmount;
+    });
+
+    leftbtn.addEventListener("click", () => {
+        document.documentElement.style.scrollBehavior = "smooth";
+        scrollcontainer.scrollLeft -= scrollAmount;
+    });
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const playButton = document.getElementById("play-button");
+//     const videoContainer = document.getElementById("video-container");
+
+//     playButton.addEventListener("click", () => {
+//       videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/-ndg1eEhEpg?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+//     });
+//   });
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const playButton = document.getElementById("play-button");
+    const videoContainer = document.getElementById("video-container");
+
+    playButton.addEventListener("click", () => {
+      videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/-ndg1eEhEpg?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+    });
+
+    document.addEventListener("mouseup", (event) => {
+      const isClickInside = videoContainer.contains(event.target);
+      if (!isClickInside) {
+        // Clicked outside the video container, close the video
+        videoContainer.innerHTML = '';
+      }
+    });
+  });
