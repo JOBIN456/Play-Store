35e279c002871cbde2c0ab3e835a7690168edfbe
JOBIN456 <jobinj5210@gmail.com>
2024-03-06 22:43:35 +0530

    game-back-end

--- googleapp/models.py
+++ googleapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Game_Category(models.Model):
+    name=models.TextField(max_length=1500)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    image=models.ImageField(upload_to='media/')
+    small_image=models.ImageField(upload_to='media/',blank=True,null=True)
+    category = models.ForeignKey(Game_Category, on_delete=models.CASCADE,blank=True,null=True)
+    vedio_code=models.CharField(max_length=400)
+    main_head=models.TextField(max_length=400)
+    sub_head=models.TextField(max_length=500)
+    rating=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.main_head
--- googleapp/views.py
+++ googleapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def base(request):
-    return render(request,"home.html")
+    windows=Game_Category.objects.get(name='windows')
+
+    window_games=Game.objects.filter(category=windows)[:6]
+    context={
+        "window_games":window_games
+    }
+    return render(request,"home.html",context)
 def phone(request):
     return render(request,"phone.html")
 def tablet(request):
@@ -15,3 +21,4 @@ def chromebook(request):
     return render(request,"chromebook.html")
 def watch(request):
     return render(request,"watch.html")
+
